546d42e7193f7a1c870c4d4a70468266a98800b3
richwolff <richwolff12@gmail.com>
2018-10-01 15:44:11 +0000

    Added ability to update profile page

--- ffanalytics/forms.py
+++ ffanalytics/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from ffanalytics.models import users
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     email = StringField('E-Mail', validators = [DataRequired(),Email()])
@@ -21,3 +23,17 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    email = StringField('E-Mail', validators = [DataRequired(),Email()])
+    first_name = StringField('First Name', validators = [DataRequired(),Length(min=2,max=20)])
+    last_name = StringField('Last Name', validators = [DataRequired(),Length(min=2,max=20)])
+    profile_picture = FileField('Update Profile Picture', validators = [FileAllowed(['jpg','png','gif'])])
+    submit = SubmitField('Update Account')
+
+    def validate_email(self,email):
+        if not email.data == current_user.email:
+            user = users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exists. Please login with your existing account.')
+
--- ffanalytics/routes.py
+++ ffanalytics/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request
 from ffanalytics import app, db, bcrypt
-from ffanalytics.forms import RegistrationForm, LoginForm
+from ffanalytics.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
-
+import secrets
+import os
+from PIL import Image
 #from datetime import dt
 # A very simple Flask Hello World app for you to get started with...
 ## IMPORT DB MODEL
@@ -12,7 +14,6 @@ from ffanalytics.models import users,user_notes,user_note_changes
 # Pages
 @app.route('/')
 def home():
-
     return render_template('home.html')
 
 
@@ -39,6 +40,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html',title='Register',form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -57,12 +59,48 @@ def login():
 
     return render_template('login.html',title='Login',form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+
+def save_picture(form_picture):
+    #Rename file name using random filename
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = str(random_hex)+ str(f_ext)
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125,125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html',title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.image_file = picture_file
+        if not form.first_name.data == current_user.first_name:
+            current_user.first_name = form.first_name.data
+        if not form.last_name.data == current_user.last_name:
+            current_user.last_name = form.last_name.data
+        if not form.email.data == current_user.email:
+            current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.email.data = current_user.email
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
